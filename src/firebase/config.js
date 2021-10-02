123003774345cdc8960e0d0b94ea642acf44184a
Moonkin <m.moonkin.z@gmail.com>
2021-10-02 17:42:14 +0200

    firebase config file created

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+import "firebase/compat/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB968LZU-jwGThoeKhvDKYGjaA5qW9scBg",
+  authDomain: "smarties-3dde8.firebaseapp.com",
+  projectId: "smarties-3dde8",
+  storageBucket: "smarties-3dde8.appspot.com",
+  messagingSenderId: "34152716835",
+  appId: "1:34152716835:web:c48d401baa26ae5d99ef96"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+const firestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+export { storage, firestore, timestamp };
