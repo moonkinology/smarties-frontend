ab923b9c421da3589f9ab58792751220bdd94fcb
Moonkin <m.moonkin.z@gmail.com>
2021-10-02 18:36:57 +0200

    show carousel in main page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import Carousel from "./components/Carousel";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <React.Fragment>
+      <div className="container">
+        <Carousel />
+      </div>
+    </React.Fragment>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 reportWebVitals();
