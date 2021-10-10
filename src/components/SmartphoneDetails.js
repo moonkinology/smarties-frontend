772f8af3eb3e2a4c51e5d9a08b12e4462290c5a0
Moonkin <m.moonkin.z@gmail.com>
2021-10-10 14:40:50 +0200

    Smartphone details basic UI component

---
+++ src/components/SmartphoneDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DetailsTabs from "./DetailsTabs";
+import DetailsMain from "./DetailsMain";
+function SmartphoneDetails() {
+  return (
+    <div>
+      <DetailsTabs />
+      <DetailsMain />
+    </div>
+  );
+}
+
+export default SmartphoneDetails;
