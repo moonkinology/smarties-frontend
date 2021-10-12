122f782e28e53030dbc52b566c2099d384dfbd1c
Moonkin <m.moonkin.z@gmail.com>
2021-10-12 19:50:14 +0200

    basic header UI component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import SmartphoneCard from "./components/SmartphoneCard";
 import AdminForm from "./components/AdminForm";
 import SignUp from "./components/SigunUp";
 import Login from "./components/Login";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
+        <Header />
         <Login />
         <SmartphoneCard />
       </div>
