a10d531cde94567357a7f35e79602657270642e2
Moonkin <m.moonkin.z@gmail.com>
2021-10-17 14:18:58 +0200

    moved header to app.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Header from "./Header";
+
 import Carousel from "./Carousel";
 function Main() {
   return (
     <div>
-      <Header />
       <Carousel />
     </div>
   );
