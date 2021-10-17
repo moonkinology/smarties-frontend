4b8526e0190b088629b73b9ccdae0ab396be43c2
Moonkin <m.moonkin.z@gmail.com>
2021-10-17 14:19:14 +0200

    basic about UI component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import AdminForm from "./components/AdminForm";
 import SignUp from "./components/auth/SigunUp";
 import Login from "./components/auth/Login";
 import Main from "./components/Main";
+import About from "./components/About";
 import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -12,10 +14,12 @@ function App() {
       <div className="container">
         <Router>
           <AuthProvider>
+            <Header />
             <Switch>
               <Route exact path="/" component={Main} />
               <Route path="/signup" component={SignUp} />
               <Route path="/login" component={Login} />
+              <Route path="/about" component={About} />
             </Switch>
           </AuthProvider>
         </Router>
