ee72f6bda8d47a818ec68818068c39d1a1f8ca48
Moonkin <m.moonkin.z@gmail.com>
2021-10-24 14:37:52 +0200

    fetch smartphone details

--- src/components/Smartphone/DetailsMain.js
+++ src/components/Smartphone/DetailsMain.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+function DetailsMain({ id }) {
+  const [details, setDetails] = useState([]);
+  const [detailsError, setDetailsError] = useState([]);
 
-function DetailsMain() {
+  const detailsCancelTokenSource = axios.CancelToken.source();
+
+  async function fetchDetails() {
+    await axios({
+      method: "get",
+      url: `http://localhost:8080/spd/${id}`,
+      cancelToken: detailsCancelTokenSource.token,
+    }).then(
+      (response) => {
+        setDetails(response.data);
+        console.log(details);
+      },
+      (error) => {
+        console.log("error");
+        setDetailsError(error);
+      }
+    );
+  }
+
+  useEffect(() => {
+    fetchDetails();
+
+    return () => {
+      detailsCancelTokenSource.cancel();
+    };
+  }, []);
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-6">
-          <table class="table">
+          <table className="table">
             <img
               src="https://www.telekom.de/resources/images/655284/samsung-galaxy-s21-5g-phantom-gray-vorne.png"
               className="d-block w-100"
@@ -13,39 +43,82 @@ function DetailsMain() {
             />
           </table>
         </div>
-        <div className="col-6">
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col" colspan="3">
-                  Important Features
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td colspan="2">Otto</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td colspan="2">Thornton</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-              </tr>
-              <tr>
-                <th scope="row">4</th>
-                <td colspan="2">Larry the Bird</td>
-              </tr>
-              <tr>
-                <th scope="row">5</th>
-                <td colspan="2">Larry the Bird</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+
+        <table className="table col-6">
+          <thead>
+            <tr>
+              <th scope="col" colSpan="6">
+                Important Features
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Platform</th>
+              <td colSpan="2">{details.platform}</td>
+            </tr>
+            <tr>
+              <th scope="row">Model</th>
+              <td colSpan="2">{details.model}</td>
+            </tr>
+            <tr>
+              <th scope="row">Main Camera</th>
+              <td colSpan="2">{details.mainCamera}</td>
+            </tr>
+            <tr>
+              <th scope="row">Front Camera</th>
+              <td colSpan="2">{details.frontCamera}</td>
+            </tr>
+            <tr>
+              <th scope="row">Battery (mAh)</th>
+              <td colSpan="2">{details.battery}</td>
+            </tr>
+
+            <tr>
+              <th scope="row">Memory </th>
+              <td colSpan="2">{details.memory}</td>
+            </tr>
+
+            <tr>
+              <th scope="row">RAM</th>
+              <td colSpan="2">{details.ram}</td>
+            </tr>
+
+            <tr>
+              <th scope="row">Release Date</th>
+              <td colSpan="2">{details.releaseDate}</td>
+            </tr>
+            <tr>
+              <th scope="row">Display Size (Inch)</th>
+              <td colSpan="2">{details.displaySize}</td>
+            </tr>
+
+            <tr>
+              <th scope="row">Display Resolution</th>
+              <td colSpan="2">{details.displayResolution}</td>
+            </tr>
+            <tr>
+              <th scope="row">Manufacturer</th>
+              <td colSpan="2">{details.manufacturer}</td>
+            </tr>
+            <tr>
+              <th scope="row">Height</th>
+              <td colSpan="2">{details.height}</td>
+            </tr>
+            <tr>
+              <th scope="row">Width</th>
+              <td colSpan="2">{details.width}</td>
+            </tr>
+            <tr>
+              <th scope="row">Depth</th>
+              <td colSpan="2">{details.depth}</td>
+            </tr>
+            <tr>
+              <th scope="row">Weight</th>
+              <td colSpan="2">{details.weight}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
