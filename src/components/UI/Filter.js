a4e9f8348d04752698c71a33c2657bf6cb86e37e
Moonkin <m.moonkin.z@gmail.com>
2021-10-24 16:40:10 +0200

    filter options

--- src/components/UI/Filter.js
+++ src/components/UI/Filter.js
@@ -1,6 +1,79 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function Filter() {
+  const [formData, setFormData] = useState({
+    platform: "",
+    priceCategory: "",
+    brand: "",
+  });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const platforms = [
+    {
+      label: "iOS",
+      value: "iOS",
+    },
+    {
+      label: "Android",
+      value: "Android",
+    },
+  ];
+
+  const prices = [
+    {
+      value: "0",
+      label: "Budget",
+    },
+    {
+      value: "1",
+      label: "Mid-Range",
+    },
+    {
+      value: "2",
+      label: "High-End",
+    },
+  ];
+  const options = [
+    {
+      label: "Apple",
+      value: "Apple",
+    },
+    {
+      label: "Samsung",
+      value: "Samsung",
+    },
+    {
+      label: "Sony",
+      value: "Sony",
+    },
+    {
+      label: "Honor",
+      value: "Honor",
+    },
+    {
+      label: "Nokia",
+      value: "Nokia",
+    },
+    {
+      label: "RealMe",
+      value: "RealMe",
+    },
+    {
+      label: "Xiaomi",
+      value: "Xiaomi",
+    },
+    {
+      label: "OnePlus",
+      value: "OnePlus",
+    },
+    {
+      label: "Huawei",
+      value: "Huawei",
+    },
+  ];
   return (
     <div>
       <button
@@ -15,12 +88,12 @@ function Filter() {
 
       <div
         className="offcanvas offcanvas-top"
-        tabindex="-1"
+        tabIndex="-1"
         id="offcanvasTop"
-        aria-labelledby="offcanvasTopLabel"
+        aria-labelledby="filterCanvasLabel"
       >
         <div className="offcanvas-header">
-          <h5 id="offcanvasTopLabel">Offcanvas top</h5>
+          <h5 id="filterCanvasLabel">Filter Options</h5>
           <button
             type="button"
             className="btn-close text-reset"
@@ -28,7 +101,87 @@ function Filter() {
             aria-label="Close"
           ></button>
         </div>
-        <div className="offcanvas-body">...</div>
+        <div className="offcanvas-body  ">
+          <form onSubmit={(e) => submitHandler(e)}>
+            <div className="d-flex justify-content-center m-3 gap-3">
+              <div className="col-2">
+                <label htmlFor="platform" className="form-label mb-3">
+                  Platform
+                </label>
+
+                <select
+                  className="form-select"
+                  id="platform"
+                  aria-label="Default select example"
+                  defaultValue={formData.platform}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      platform: e.target.value,
+                    });
+                  }}
+                >
+                  <option defaultValue></option>
+                  {platforms.map((platform) => (
+                    <option key={platform.value} defaultValue={platform.value}>
+                      {platform.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="col-2">
+                <label htmlFor="priceCategory" className="form-label mb-3">
+                  Price Category
+                </label>
+                <select
+                  className="form-select"
+                  id="priceCategory"
+                  aria-label="Default select example"
+                  defaultValue={formData.priceCategory}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      priceCategory: e.target.value,
+                    });
+                  }}
+                >
+                  <option defaultValue></option>
+                  {prices.map((price) => (
+                    <option key={price.value} defaultValue={price.value}>
+                      {price.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="col-2">
+                <label htmlFor="brand" className="form-label mb-3">
+                  brand
+                </label>
+                <select
+                  className="form-select"
+                  id="brand"
+                  aria-label="Default select example"
+                  defaultValue={formData.brand}
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      brand: e.target.value,
+                    });
+                  }}
+                >
+                  <option defaultValue></option>
+                  {options.map((option) => (
+                    <option key={option.value} defaultValue={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
