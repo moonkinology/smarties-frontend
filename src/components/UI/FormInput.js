b98ff6e5c3a5ceb7d897e8053e06b65328bf7631
Moonkin <m.moonkin.z@gmail.com>
2021-12-17 15:28:42 +0100

    changed form input impl

--- src/components/UI/FormInput.js
+++ src/components/UI/FormInput.js
@@ -1,26 +1,28 @@
-import React from 'react'
+import React from "react";
 
-function FormInput({ changeCallBack, inputType, defValue, colMdSize, label }) {
-    return (
-        <div className={colMdSize}>
-            <label htmlFor="customInput" className="form-label">
-                {label}
-            </label>
-            <input
-                required
-                type={inputType}
-                className="form-control"
-                id="customInput"
-                defaultValue={defValue}
-                onChange={(e) => {
-                    setFormData({
-                        ...formData,
-                        frontCamera: e.target.value,
-                    });
-                }}
-            />
-        </div>
-    )
+function FormInput({
+  changeCallBack,
+  inputType,
+  defValue,
+  colMdSize,
+  label,
+  obj,
+}) {
+  return (
+    <div className={colMdSize}>
+      <label htmlFor="customInput" className="form-label">
+        {label}
+      </label>
+      <input
+        required
+        type={inputType}
+        className="form-control"
+        id="customInput"
+        defaultValue={defValue}
+        onChange={(e) => changeCallBack(e.target.value, obj)}
+      />
+    </div>
+  );
 }
 
-export default FormInput
+export default FormInput;
