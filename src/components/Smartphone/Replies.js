a865e318303b0a621181ce3953bdd2502da055e2
Moonkin <m.moonkin.z@gmail.com>
2022-02-15 20:25:59 +0100

    fixed lint error

--- src/components/Smartphone/Replies.js
+++ src/components/Smartphone/Replies.js
@@ -29,11 +29,11 @@ function Replies({
         viewBox="0 0 16 16"
       >
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z"
         />
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
         />
       </svg>
@@ -47,11 +47,11 @@ function Replies({
         viewBox="0 0 16 16"
       >
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
         />
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
         />
       </svg>
